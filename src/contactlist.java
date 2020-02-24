518aa3ba0d72191b3d7b3a9d03b88bc24eb08621
AnushkaPandey2 <anushka.pandey_cs18@gla.ac.in>
2020-02-24 15:58:40 +0530

    delete and printlist method

--- src/contactlist.java
+++ src/contactlist.java
@@ -37,6 +37,58 @@ public class contactlist {
         }
         return response;
     }
-
+    public void printlist()
+    {
+        System.out.println("printing contact list");
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.getDataname()+" : "+temp.getDatacontactno()+" \n");
+            temp=temp.getNext();
+        }
+        System.out.println();
     }
+    public int delete(String dataname) {
+        int response = 0;
+        int c = 0;
+        if (head != null) {
+            Node temp = head;
+            Node temp2;
+            // Node previous=null;
+            while (temp != null) {
+                if (temp.getDataname().equals(dataname) && temp == head && temp.getNext() != null) {
+                    head = temp.getNext();
+                    head.setPrevious(null);
+                    response = 1;
+                    c = 1;
+                    break;
+                } else if (temp.getDataname().equals(dataname) && temp != head && temp.getNext() == null) {
+                    temp = temp.getPrevious();
+                    temp.setNext(null);
+                    response = 1;
+                    c = 1;
+                    break;
+                } else if (temp.getDataname().equals(dataname) && temp == head && temp.getNext() == null) {
+                    head = null;
+                    response = 1;
+                    c = 1;
+                    break;
+                } else if (temp.getDataname().equals(dataname) && temp != head && temp.getNext() != null) {
+                    temp2 = temp;
+                    temp = temp.getPrevious();
+                    temp.setNext(temp2.getNext());
+                    temp2.getNext().setPrevious(temp);
+                    response = 1;
+                    c = 1;
+                    break;
+                }
+                temp = temp.getNext();
+            }
+            if (c == 0) {
+                System.out.println("element not found");
+            }
+        }
+        return response;
+
+    }}
 
